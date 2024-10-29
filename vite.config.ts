37db9cce1b0df4778df03a3e8d8b184bbce880e2
Tu Anh <june265zz@gmail.com>
2024-10-29 16:41:17 -0400

    change base in vite.config.ts. to deploy on vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/portfolio/',
+  //base: '/portfolio/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
