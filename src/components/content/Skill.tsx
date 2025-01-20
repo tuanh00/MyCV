6b2cc80e82c9a599f4799d5ae55e09f7b7abef43
Tu Anh <june265zz@gmail.com>
2025-01-20 17:31:10 -0500

    Updated Skill.tsx

--- src/components/content/Skill.tsx
+++ src/components/content/Skill.tsx
@@ -5,7 +5,7 @@ const Skill = () => {
     return (
         <>
         <div className="arlo_tm_section" id="skills">
-            {/* -----skill_1----- */}
+            {/* -----Front-end Skills----- */}
                 <div className="arlo_tm_skills_wrap">
                   <div className="container">
                   <div className="arlo_tm_title_holder">
@@ -17,18 +17,29 @@ const Skill = () => {
                         <div className="arlo_tm_mini_title_holder">
                           <h4>Front-end Skills</h4>
                         </div>
-                        <p>For the front-end, HTML and CSS provided a solid foundation for kick-starting my journey into web development.<br/><br/> Moving forward, I used Vite and React to streamline the development process, making it easier to collaborate with teammates and build efficient applications. <br/><br/>Additionally, Figma has been invaluable in allowing me to deliver clear visual designs that align with customer expectations.</p>                   
+                        <div className="skill_description">
+                          <p>
+                            For the front-end, <strong>HTML</strong> and <strong>CSS</strong> provided a solid 
+                            foundation for kick-starting my journey into web development.<br /><br />
+                            Moving forward, I used <strong>Vite</strong> and <strong>React</strong> to streamline 
+                            the development process, making it easier to collaborate with teammates and build 
+                            efficient applications.<br /><br />
+                            Additionally, <strong>Figma</strong> has been invaluable in allowing me to deliver 
+                            clear visual designs that align with customer expectations.
+                          </p>
+                      </div>
                           </div>
                       <div className="rightbox">
                         <div className="progress_bar_wrap_total">
                           <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
                             <div className="flex-container">
                               <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" alt="html_icon"/>
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" alt="css_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" alt="react_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg" alt="react_bootstrap_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" alt="vite_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" alt="figma_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swift/swift-original.svg" alt="swift_icon" />
                             </div>
                             {/* <div className="arlo_tm_progress" data-value="95" data-color="#000">
                               <span>
@@ -78,8 +89,8 @@ const Skill = () => {
                     </div>
                   </div>
                 </div>
-            {/* ----- end skill_1----- */}
-    {/* -----skill_1----- */}
+            {/* ----- end Front-end Skills----- */}
+    {/* -----Back-end Skills----- */}
     <div className="arlo_tm_skills_wrap">
                   <div className="container">
                     <div className="inner_wrap">
@@ -87,20 +98,38 @@ const Skill = () => {
                         <div className="arlo_tm_mini_title_holder">
                           <h4>Back-end Skills</h4>
                         </div>
-                        <p>On the back-end, I leverage logic-based languages to handle complex functionalities and use tools like SQL to store and manage data efficiently.<br/><br/>Git helps me track and preserve project versions, ensuring smooth collaboration across teams.<br/><br/>For API documentation and management, Swagger allows me to maintain organized endpoints, ensuring integration for development teams.</p>
+                        <div className="skill_description">
+                        <p>
+                        On the back-end, I use languages like <strong>Java</strong>, <strong>C#</strong>, <strong>JavaScript</strong>, and <strong>Python</strong> to handle complex server-side logic. 
+                        I also leverage <strong>Jupyter</strong> notebooks for interactive data visualization, charting, and presentations, 
+                        while <strong>Spyder</strong> helps me quickly prototype and debug Python code.<br/><br/>
+                        <strong>SQL</strong> provides efficient relational data storage, 
+                        and <strong>Firebase</strong> offers real-time database capabilities.<br/><br/>
+                        <strong>Git</strong> helps me track and preserve project versions, 
+                        ensuring smooth collaboration across teams.<br/><br/>
+                        For API documentation and management, <strong>Swagger</strong> allows me to maintain organized endpoints, 
+                        ensuring seamless integration for development teams.
+                      </p>
+                        </div>
+
                       </div>
                       <div className="rightbox">
                         <div className="progress_bar_wrap_total">
                           <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
                           <div className="flex-container">
 
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />   
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />       
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original-wordmark.svg" />    
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" />
-                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original-wordmark.svg" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" alt="java_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" alt="csharp_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="js_icon" /> 
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" alt="python_icon" />  
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original-wordmark.svg" alt="sqlserver_icon" /> 
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-plain-wordmark.svg" alt="firebase_icon" /> 
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg" alt="jupyter_icon" />
+                              <img className="responsive-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Spyder_logo.svg/120px-Spyder_logo.svg.png" alt="spyder_icon" style={{ maxHeight: "60px" }} /> 
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" alt="node_icon" />        
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg"alt="git_icon" />
+                              <img className="responsive-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original-wordmark.svg" alt="swagger_icon" />
+                             
                             </div>
                           </div>
                         </div>
@@ -108,7 +137,7 @@ const Skill = () => {
                     </div>
                   </div>
                 </div>
-            {/* ----- end skill_1----- */}
+            {/* ----- end Back-end Skills----- */}
               </div>
         </>
     );
